0b264b8d9b11b63aa3b93ebc0779c10e8fb3b679
idsubhasish <95999921+idsubhasish@users.noreply.github.com>
2022-02-20 18:16:16 +0530

    Changes Of Bot Start Info

--- bot/__main__.py
+++ bot/__main__.py
@@ -59,8 +59,9 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("⚜️ STARTER MIRROR AND LEECH ⚜️", "https://t.me/StarterLeech")
+    buttons.buildbutton("💠 PART OF", "https://t.me/StarterNetworkZ")
+    buttons.buildbutton("MAINTAINED BY 🗣", "https://t.me/Harshu_XD")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
