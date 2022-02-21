2963c76f01814b41f1fcec451f0d2b8a1103d5b2
idsubhasish <95999921+idsubhasish@users.noreply.github.com>
2022-02-21 21:51:16 +0530

    Silly Changes Of Unicode 😋

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -163,7 +163,7 @@ def get_readable_message():
                 except:
                     pass
                 msg += f'\n<b>⌈➳○ Adder :</b> <i>{download.message.from_user.first_name}</i> (<code>{download.message.from_user.id}</code>)'
-                msg += f"\n<b>⌈➳○ To Stop :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<b>⌊➳○ To Stop :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
                 msg += f"\n<b>⌈➳○ Size : </b>{download.size()}"
                 msg += f"\n<b>⌈➳○ Speed : </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
@@ -171,7 +171,7 @@ def get_readable_message():
                 msg += f"\n<b>⌈➳○ Ratio : </b>{round(download.torrent_info().ratio, 3)}"
                 msg += f" | <b>⌈➳○ Time : </b>{get_readable_time(download.torrent_info().seeding_time)}"
                 msg += f'\n<b>⌈➳○ Adder :</b> <i>{download.message.from_user.first_name}</i> (<code>{download.message.from_user.id}</code>)'
-                msg += f"\n<b>⌈➳○ To Stop :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<b>⌊➳○ To Stop :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             else:
                 msg += f"\n<b>Size: </b>{download.size()}"
             msg += "\n\n"
